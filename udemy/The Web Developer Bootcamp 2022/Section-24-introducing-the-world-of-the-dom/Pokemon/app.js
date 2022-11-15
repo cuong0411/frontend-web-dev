0c8305e8a8654efff06a61724b8393a7a5a532fa
nguyen cuong <cuong0411@outlook.com>
2022-11-15 13:33:18 +0700

    pokemon exercise

--- udemy/The Web Developer Bootcamp 2022/Section-24-introducing-the-world-of-the-dom/Pokemon/app.js
+++ udemy/The Web Developer Bootcamp 2022/Section-24-introducing-the-world-of-the-dom/Pokemon/app.js
@@ -1,19 +1,20 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+let link = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 100; i++) {
     const pokemon = document.createElement('div');
+    const spanEle = document.createElement('span');
+    const img = document.createElement('img');
+
+    spanEle.innerText = `#${i}`;
+    img.src = `${link}${i}.png`;
+
     pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+    // img.classList.add('pokemon img');
 
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    pokemon.appendChild(img);
+    pokemon.appendChild(spanEle);
+    container.appendChild(pokemon);
 }
