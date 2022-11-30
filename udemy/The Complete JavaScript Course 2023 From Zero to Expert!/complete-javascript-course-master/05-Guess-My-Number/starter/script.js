c3721b175b368304a80f097c63502362687f7dc3
nguyen cuong <cuong0411@outlook.com>
2022-11-30 13:29:24 +0700

    guess a number exercise (not done)

--- udemy/The Complete JavaScript Course 2023 From Zero to Expert!/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ udemy/The Complete JavaScript Course 2023 From Zero to Expert!/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 10;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  document.querySelector('.message').textContent = '🎉Correct number';
+});
