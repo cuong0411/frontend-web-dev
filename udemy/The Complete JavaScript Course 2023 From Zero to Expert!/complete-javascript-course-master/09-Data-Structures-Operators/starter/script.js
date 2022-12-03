cd7a196948e5c9bead2f1c208967ae35399be889
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2022-12-03 17:47:27 +0700

    destructuring arrays

--- udemy/The Complete JavaScript Course 2023 From Zero to Expert!/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ udemy/The Complete JavaScript Course 2023 From Zero to Expert!/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,25 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+const nested = [1, 2, [3, 4]];
+const [a, , [b, c]] = nested;
+console.log(a, b, c);
+
+// default value
+const arr1 = [10];
+const [q = 1, w = 2, e = 3] = arr1;
+console.log(q, w, e);
